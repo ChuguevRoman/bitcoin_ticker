abeb9cf025e2e158ec5f2f396bd4e765ebaa669c
ChuguevRoman <5tiger5xman@gmail.com>
2023-03-08 14:46:18 +0200

    Connected the http package. In coin_data, I created a getCoinData method that will make a request to the server and return a value for bitcoin, depending on the selected currency.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,3 +31,19 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+
+const apiKey = '11D2DDB4-D6A9-47D7-A170-E2508447746B';
+const coinAPIUrl = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future<String> getCoinData(String currency) async {
+    http.Response response = await http.get(
+      Uri.parse('$coinAPIUrl/BTC/$currency?apikey=$apiKey'),
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['rate'].toStringAsFixed(0);
+    } else {
+      throw 'Problem with the get request.\nError ${response.statusCode}';
+    }
+  }
+}
