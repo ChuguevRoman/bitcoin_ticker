53b44a173c690adb194e4f084ecf6d480662fb14
ChuguevRoman <5tiger5xman@gmail.com>
2023-03-09 10:28:01 +0200

    In coin_data, let's change the getCoinData method so that it returns values for three cryptocurrencies.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -36,14 +36,18 @@ const apiKey = '11D2DDB4-D6A9-47D7-A170-E2508447746B';
 const coinAPIUrl = 'https://rest.coinapi.io/v1/exchangerate';
 
 class CoinData {
-  Future<String> getCoinData(String? currency) async {
-    http.Response response = await http.get(
-      Uri.parse('$coinAPIUrl/BTC/$currency?apikey=$apiKey'),
-    );
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body)['rate'].toStringAsFixed(0);
-    } else {
-      throw 'Problem with the get request.\nError ${response.statusCode}';
+  Future<dynamic> getCoinData(String? currency) async {
+    Map<String, String> cryptoValue = {};
+    for (String crypto in cryptoList) {
+      http.Response response = await http
+          .get(Uri.parse('$coinAPIUrl/$crypto/$currency?apikey=$apiKey'));
+      if (response.statusCode == 200) {
+        cryptoValue[crypto] =
+            jsonDecode(response.body)['rate'].toStringAsFixed(0);
+      } else {
+        throw 'Problem with the get request.\nError ${response.statusCode}';
+      }
     }
+    return cryptoValue;
   }
 }
